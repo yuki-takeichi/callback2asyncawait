53bb02d2ac392ab8d795b0078d8d6c4145c49c1c
Yuki Takeichi <yuki.takeichi@gmail.com>
2018-05-11 21:34:07 +0900

    wip

--- traverse.js
+++ traverse.js
@@ -3,12 +3,14 @@ const util = require('util');
 const esprima = require('esprima');
 const estraverse = require('estraverse');
 
-const path = './simple/cpsed.js';
+const path = './simple/cps.js';
 const code = fs.readFileSync(path, 'utf8');
 const ast = esprima.parse(code);
 
 const transform = require('./lib/transform');
 const inspect = obj => console.log(util.inspect(obj, false, null));
+
+inspect(ast);
 
 estraverse.traverse(ast, {
   enter: (node, parent) => {
